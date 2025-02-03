57cdf2ae50beb9fc5cf681980380fb284eae2e7e
Levin-Alexey <levin.penza@yandex.ru>
2025-02-03 12:23:29 +0300

    start pr

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import logging
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, ConversationHandler
 
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 ASK_NAME, ASK_AGE = range(2)
-
 
 async def start(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text(
@@ -20,26 +17,22 @@ async def start(update: Update, context: CallbackContext) -> int:
     )
     return ASK_NAME
 
-
 async def contact_handler(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text('Спасибо! Как вас зовут?')
     return ASK_NAME
 
-# Функция для обработки имени пользователя
 async def name_handler(update: Update, context: CallbackContext) -> int:
     user_name = update.message.text
     context.user_data['name'] = user_name
     await update.message.reply_text(f'Приятно познакомиться, {user_name}! Сколько лет Вашему ребенку?')
     return ASK_AGE
 
-# Функция для обработки возраста ребенка
 async def age_handler(update: Update, context: CallbackContext) -> int:
     child_age = update.message.text
     await update.message.reply_text(f'Спасибо за информацию! В ближайшее время мы с Вами свяжемся!')
     return ConversationHandler.END
 
 def main() -> None:
-    # Вставьте сюда ваш токен
     application = Application.builder().token("").build()
 
     conv_handler = ConversationHandler(
